b8c7fbba05a62225e87cec8648addb65f2ee4cab
Margarita Morozova <ritamorozova@yahoo.com>
2020-07-06 21:37:36 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -25,8 +25,8 @@ end
 def initial_round
   a = deal_card
   b = deal_card
-  card_total = sum(card_one, card_two)
-  display_card_total(card_total)
+  card_total = sum(a,b)
+  return display_card_total(card_total)
 end
 
 def hit?
