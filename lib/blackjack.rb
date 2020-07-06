cc6531e09a4f5a582d474dc15c8e7e5378a99208
Margarita Morozova <ritamorozova@yahoo.com>
2020-07-06 21:43:10 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -23,9 +23,7 @@ def end_game(card_total)
 end
 
 def initial_round
-  card_one = deal_card
-  card_two = deal_card
-  card_total = card_one + card_two
+  card_total = deal_card + deal_card
   display_card_total(card_total)
   return card_total
 end
